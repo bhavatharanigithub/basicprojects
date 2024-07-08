719c6c392504b6bec4534553768e14477f3bb846
bhavatharanigithub <bhavatharni.j@gmail.com>
2024-07-08 20:59:47 +0530

    Update Maths operator generator.py

--- Maths operator generator.py
+++ Maths operator generator.py
@@ -39,4 +39,4 @@ total_time = round(end_time - start_time, 2)
 
 print("----------------------")
 print("Nice work! You finished in", total_time, "seconds!")
-print('You answerd',wrong,"wrong answer")
+print('You answered',wrong,"wrong answer")
